34810201d50d72986771d1e6541400bf157e4aa8
SeneshAnujaya <senesh9637@gmail.com>
2024-07-26 18:57:08 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,19 +2,24 @@ import { useRef, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import {app} from '../firebase';
+import { useDispatch } from "react-redux";
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess } from "../redux/user/userSlice";
+import { deleteUser } from "../../../api/controllers/user.controller";
 
 
 const Profile = () => {
+  const dispatch = useDispatch();
   const fileRef = useRef(null);
   const [image, setImage] = useState(undefined);
   const [imagePercent, setImagePercent] = useState(0);
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
   
 
 
 
-  const { currentuser } = useSelector((state) => state.user);
+  const { currentuser, loading, error } = useSelector((state) => state.user);
 
   useEffect(() => {
     if(image) {
@@ -42,13 +47,57 @@ const Profile = () => {
   );
   }
 
- 
+ const handleChange = (e) => {
+  setFormData({...formData, [e.target.id]: e.target.value});
+ };
+
+ const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    dispatch(updateUserStart());
+    const res = await fetch(`/api/user/update/${currentuser._id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    if(data.success === false) {
+      dispatch(updateUserFailure(data));
+      return;
+    }
+    dispatch(updateUserSuccess(data));
+    setUpdateSuccess(true);
+  } catch (error) {
+    dispatch(updateUserFailure(error));
+  }
+ }
+
+ const handleDeleteAccount = async () => {
+  
+  try {
+    dispatch(deleteUserStart());
+    const res = await fetch(`/api/user/delete/${currentuser._id}`, {
+      method: 'DELETE'
+    });
+    const data = await res.json();
+    if(data.success === false) {
+      dispatch(deleteUserFailure(data));
+      return;
+    }
+    dispatch(deleteUserSuccess(data));
+  } catch (error) {
+    dispatch(deleteUserFailure(error));
+  }
+ }
+
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
 
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input type="file" ref={fileRef} hidden accept="image/*" onChange={(e) => setImage(e.target.files[0])}/>
         <img
           src={formData.profilePicture || currentuser.profilePicture}
@@ -62,6 +111,7 @@ const Profile = () => {
           id="username"
           placeholder="Username"
           className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
         />
 
         <input
@@ -70,6 +120,7 @@ const Profile = () => {
           id="email"
           placeholder="Email"
           className="bg-slate-100 rounded-lg p-3"
+onChange={handleChange}
         />
 
         <input
@@ -77,13 +128,16 @@ const Profile = () => {
           id="password"
           placeholder="password"
           className="bg-slate-100 rounded-lg p-3"
+onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Update</button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteAccount} className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
+      <p className="text-red-700 mt-5">{error && 'Something went wrong!'}</p>
+      <p className="text-green-700 mt-5">{updateSuccess && 'user is updated successfully!'}</p>
     </div>
   );
 };
